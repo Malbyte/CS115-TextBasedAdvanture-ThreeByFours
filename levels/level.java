58adce4d2aba22e525b564b424d5c46cc5866b47
jgoldstei@hartford.edu <jgoldstei@hartford.edu>
2024-03-19 10:03:57 -0400

    add skeleton of level class

---
+++ levels/level.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class level {
+	private ArrayList<int> curLevel;
+	
+	public void level(){
+		curLevel = null;
+	}
+	public void loadMap(){
+		
+	}
+	
+	public void printMap(){
+		//clears the screen and displays a viewport of the level
+	}
+	public void getTile(){
+		
+	}
+}
