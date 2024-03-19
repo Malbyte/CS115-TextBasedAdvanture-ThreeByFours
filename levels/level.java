3d595b7814975ddae9c9bbe61ea45da55b60460c
jgoldstei@hartford.edu <jgoldstei@hartford.edu>
2024-03-19 10:14:29 -0400

    implement basic interactions
    implemented basic interactions with arraylist map structure

--- levels/level.java
+++ levels/level.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.io.*;
 
 class level {
 	private ArrayList<int> curLevel;
@@ -6,14 +7,24 @@ class level {
 	public void level(){
 		curLevel = null;
 	}
-	public void loadMap(){
+	public void loadMap(String mapName){
 		
 	}
 	
 	public void printMap(){
 		//clears the screen and displays a viewport of the level
 	}
-	public void getTile(){
+	public int getTile(int pos[2]){
+		return curLevel.get(y).get(x);
+	}
+	public Boolean moveTile(int oldPos[2], int newPos[2]){
+		int tileVal = 0;
 		
+		tileVal = curLevel.get(y).get(x);
+		if(tileVal != 0)
+			return false;
+		
+		curLevel.get(y).set(x, tileVal);
+		return true;
 	}
 }
