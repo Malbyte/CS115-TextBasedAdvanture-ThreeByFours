d6f65979f5b3e3c7aa2ec2e4e58941c515d59c1d
rw555 <157328874+rw555@users.noreply.github.com>
2024-03-21 09:25:30 -0400

    finished item.java constructor

--- items/item.java
+++ items/item.java
@@ -1,8 +1,10 @@
 public class item{
 	protected int itemId, itemDamage, itemDurability;
 
-	public item(int id, int) {
-
+	public item(int id, int dmg, int dur) {
+		itemId = id;
+		itemDamage = dmg;
+		itemDurablity = dur;
 	}
 	public void damageDurability(int dmg) {
 		itemDurability -= dmg;
