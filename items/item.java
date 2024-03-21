bcf08ad3b8fcb185c414eb50ac705fdc47de5529
rw555 <157328874+rw555@users.noreply.github.com>
2024-03-21 09:22:25 -0400

    Condensed fields for item.java and fixed space typo

--- items/item.java
+++ items/item.java
@@ -1,15 +1,16 @@
 public class item{
-	protected int itemId = 0;
-	protected int itemDamage = 0;
-	protected int itemDurability = 1;
+	protected int itemId, itemDamage, itemDurability;
 
+	public item(int id, int) {
+
+	}
 	public void damageDurability(int dmg) {
 		itemDurability -= dmg;
 	}
 	public int getId() {
 		return itemId;
 	}
-	public  int getDamage() {
+	public int getDamage() {
 		return itemDamage;
 	}
 	public int getDurability() {
