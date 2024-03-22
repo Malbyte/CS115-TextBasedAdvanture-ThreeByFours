f2a5f93e4790962bc79d17add4570c9738f8b65d
rw555 <157328874+rw555@users.noreply.github.com>
2024-03-22 11:45:33 -0400

    added main for testing and explanatory comments to item generator

--- items/ItemGenerator.java
+++ items/ItemGenerator.java
@@ -1,9 +1,13 @@
 public class ItemGenerator {
+	// Main methood for testing purposes
+	public static void main(String[] args) {
+	}
+
 	public static item generate(int id) {
 		item i;
 		switch id:
 			case 0:
-				i = new stick(0, -1, 1);
+				i = new stick(id, -1, 1); // itemName(itemId, healing/damage, durability)
 				break;
 		return i;
 	}
