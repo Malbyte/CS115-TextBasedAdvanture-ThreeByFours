4dabd8b2c54a95d3ca7e5f5cdc7570dea7c9855c
rw555 <157328874+rw555@users.noreply.github.com>
2024-03-22 11:58:46 -0400

    compiled and tested generator

--- items/ItemGenerator.java
+++ items/ItemGenerator.java
@@ -1,14 +1,17 @@
 public class ItemGenerator {
 	// Main methood for testing purposes
 	public static void main(String[] args) {
+		item stck = generate(0);
+		System.out.println(stck.getDamage());
 	}
 
 	public static item generate(int id) {
-		item i;
-		switch id:
+		item i = null;
+		switch (id){
 			case 0:
 				i = new stick(id, -1, 1); // itemName(itemId, healingOrDamage, durability)
 				break;
+		}
 		return i;
 	}
 }
