454e574dcf83c988084848839071a2a94de59420
rw555 <157328874+rw555@users.noreply.github.com>
2024-03-22 13:24:58 -0400

    fixed sword constructors

--- items/sword.java
+++ items/sword.java
@@ -2,11 +2,11 @@ public class sword extends item {
 	//////////////////////////////////////////////////////////////////////////
 	// Creates a sword item
 	//////////////////////////////////////////////////////////////////////////
-	public stick(int id, int dmg, int dur) {
+	public sword(int id, int dmg, int dur) {
 		super(id, dmg, dur, "Sword");
 	}
 	// Overloaded constructor for custom sword names
-	public stick(int id, int dmg, int dur, String name) {
+	public sword(int id, int dmg, int dur, String name) {
 		super(id, dmg, dur, name);
 	}
 }
