f7631bff01cf426fbf7e42024aed0a97c28ef497
SparkBreaker2994 <164025670+SparkBreaker2994@users.noreply.github.com>
2024-04-29 17:56:46 -0400

    Update EnemyA.java

--- entities/EnemyA.java
+++ entities/EnemyA.java
@@ -7,14 +7,14 @@ public class EnemyA
 	public static void main(String[]args)
 	{
 		int abhp, ahp, aa, el, e, i;
-		int enemyLevel = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+		int enemyLevel = {1, 2, 3, 4, 5, 6, 7, 8};
 		int[] random = new int[];
 		Random ran = new Random();
-		int enemyHealth = {20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95};
-		int enemyAttack = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80};
+		int enemyHealth = {20, 25, 30, 35, 40, 45, 50, 55};
+		int enemyAttack = {5, 10, 15, 20, 25, 30, 35, 40};
 		for(i = 0; i < 1; e++)
 		{
-			random[e] = ran.nextInt(16);
+			random[e] = ran.nextInt(8);
 			el = random[e]
 			abhp = enemyHealth[el]
 			int ahp = abhp;
