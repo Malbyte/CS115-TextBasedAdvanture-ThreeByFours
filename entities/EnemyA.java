9591b1954440d2870d55b2083d855120e696bf53
SparkBreaker2994 <164025670+SparkBreaker2994@users.noreply.github.com>
2024-04-29 20:16:22 -0400

    Update EnemyA.java

--- entities/EnemyA.java
+++ entities/EnemyA.java
@@ -2,24 +2,20 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class EnemyA
+public class EnemyA extends enemy
 {
-	public static void main(String[]args)
+	public static void enemies(String[]args)
 	{
-		int abhp, ahp, aa, el, e, i;
-		int enemyLevel = {1, 2, 3, 4, 5, 6, 7, 8};
-		int[] random = new int[];
+		int aa, el, i;
+		i = 0;
+		int[] enemyLevel = {1, 2, 3, 4, 5, 6, 7, 8};
 		Random ran = new Random();
-		int enemyHealth = {20, 25, 30, 35, 40, 45, 50, 55};
-		int enemyAttack = {5, 10, 15, 20, 25, 30, 35, 40};
-		for(i = 0; i < 1; e++)
-		{
-			random[e] = ran.nextInt(8);
-			el = random[e]
-			abhp = enemyHealth[el]
-			int ahp = abhp;
-			aa = enemyAttack[el];
-		}
-
+		int[] enemyHealth = {20, 25, 30, 35, 40, 45, 50, 55};
+		int[] enemyAttack = {5, 10, 15, 20, 25, 30, 35, 40};
+		int e = ran.nextInt((8 - 1)+1);
+		System.out.println(e);
+		int abhp = enemyHealth[e];
+		int ahp = abhp;
+		aa = enemyAttack[e];
 	}
 }
